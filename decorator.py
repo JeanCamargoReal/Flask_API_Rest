4ead3ee31b21e946fd5339b2aad437a35aefe9ba
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-11-22 16:41:42 -0300

    Decorators

---
+++ decorator.py
@@ -0,0 +1,21 @@
+import time
+
+
+def duration_time(function):
+    def wrapper():
+        init = time.time()
+        function()
+        final = time.time()
+
+        print('O tempo total da função {} foi de {}'.format(function.__name__, str(final - init)))
+
+    return wrapper
+
+
+@duration_time
+def main():
+    for i in range(1, 1000000):
+        pass
+
+
+main()
